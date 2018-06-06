59f98b3b647672654e1340cab3448b4627d57c34
xiaoguozi <527768758@qq.com>
2018-06-06 13:25:45 +0800

    workspace.xml

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Created by MintQ on 2018/6/6.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain=true)
+public class Dept implements Serializable {     //Dept（Entity） orm mysql->Dept (table) 对象关系映射 //必须序列化
+
+    private Long 	deptno; // 主键
+    private String 	dname; // 部门名称
+    private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+    public Dept(String dname)
+    {
+        super();
+        this.dname = dname;
+    }
+
+    public static void main(String[] args) {
+        Dept dept = new Dept();
+        dept.setDb_source("DB01").setDname("RD").setDeptno(12L);
+        System.out.println(dept.toString());
+    }
+
+}
