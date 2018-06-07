9bfb02225d6b43726dd31eb2a910c0d738d5dfdc
xiaoguozi <527768758@qq.com>
2018-06-07 18:41:35 +0800

    eureka

--- microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -4,6 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ *
+ * 注意：
+ *  Eureka的自我保护：一句话：某时刻某一个微服务不可用了，eureka不会立刻清理，依旧会对该微服务的信息进行保存。
+ *
+ *  在自我保护模式中，Eureka Server会保护服务注册表中的信息，不再注销任何服务实例。当它收到的心跳数重新恢复到阈值以上时，该Eureka Server节点就会自动退出自我保护模式。
+ *  它的设计哲学就是宁可保留错误的服务注册信息，也不盲目注销任何可能健康的服务实例。 一句话讲解： 好死不如赖活着
+ *
+ *  综上，自我保护模式是一种应对网络异常的安全保护措施。 它的架构哲学是宁可同时保留所有微服务（健康的微服务和不健康的微服务都会保留），也不盲目注销任何健康的微服务。
+ *  使用自我保护模式，可以让Eureka集群更加的健壮、稳定。
+ *
+ * 在Spring Cloud中，可以使用eureka.server.enable-self-preservation=false  禁用自我保护模式。
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
 public class EurekaServer7001_App
--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,9 @@ public class DeptController
 {
 	@Autowired
 	private DeptService service;
-//	@Autowired
-//	private DiscoveryClient client;
+	@Qualifier("discoveryClient")
+	@Autowired
+	private DiscoveryClient client;
 
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept)
@@ -38,18 +40,18 @@ public class DeptController
 	
 //	@Autowired
 //	private DiscoveryClient client;
-//	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
-//	public Object discovery()
-//	{
-//		List<String> list = client.getServices();
-//		System.out.println("**********" + list);
-//
-//		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
-//		for (ServiceInstance element : srvList) {
-//			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
-//					+ element.getUri());
-//		}
-//		return this.client;
-//	}
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
 
 }
